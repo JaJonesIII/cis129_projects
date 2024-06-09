487be8036445ce4f5c879c609e6f92dc329f423a
JaJonesIII <jjonesiii@mail.pima.edu>
2024-06-09 00:22:55 -0700

    Update cis129_lab02_shortStory.py
    Added comments on top and a short story about two friends fishing at a lake.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+# Author: Jerry Jones III
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+
+print("On a crisp winter morning, two friends made their way to the lake.")
+print("Two friends out to enjoy eachothers company and all the lake has to offer.")
+print("Not another person in sight. Just the sounds of ducks making their way around the lake")
+print("The two friends set up their boat and begin trolling around the lake. They begin casting their lines and start to catch fish.")
+print("After six hours on the lake and a stringer full of fish, they decide to pack up and make their way home and have a delicious dinner.")
+
